aa099000b3ee5697e24727dd74af5bf963b842ca
joaoazevedocavalcanti <joaoazevedocavalcanti@gmail.com>
2026-03-18 20:38:04 -0300

    questao1_lista3

---
+++ questao1.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int main(){
+    //entrada numero de casos
+    int numero_casos;
+    scanf("%d", &numero_casos);
+
+    //laço para os casos
+    for (int casos = 0; casos < numero_casos; casos++){
+        //entrada tamanho array
+        int tamanho_array;
+        scanf("%d", &tamanho_array);
+
+        int sequencia[tamanho_array]; //definindo o array
+
+        for (int i = 0; i < tamanho_array; i++){
+            scanf("%d", &sequencia[i]);
+        }
+        
+        //variaveis auxiliares da verificação
+        int inicio_geral = 0;
+        int tamanho_geral = 0;
+        int inicio_atual = 0;
+        int tamanho_atual = 0;
+
+        //laço verificação da sequencia
+        for (int i = 1; i < tamanho_array; i++){
+            if (sequencia[i] < sequencia[i - 1]){
+                tamanho_atual += 1;
+            } else {
+                if (tamanho_atual > tamanho_geral){
+                    tamanho_geral = tamanho_atual;
+                    inicio_geral = inicio_atual;
+                }
+                inicio_atual = i;
+                tamanho_atual = 0;
+            }
+        }
+
+        if (tamanho_atual > tamanho_geral){
+                tamanho_geral = tamanho_atual;
+                inicio_geral = inicio_atual;
+        }
+
+        //caso exista sequencia
+        if (tamanho_geral > 0){
+            tamanho_geral += 1; //aumentar para corrigir
+            printf("%d\n", tamanho_geral);
+            for (int i = inicio_geral; i < inicio_geral + tamanho_geral; i++){
+                if (i == inicio_geral + tamanho_geral - 1){
+                    printf("%d \n", sequencia[i]);
+                } else {
+                    printf("%d ", sequencia[i]);
+                }
+            }
+        } 
+        else { //caso não exista sequencia
+            printf("%d\n", tamanho_geral);
+        }
+            
+
+    }
+}
