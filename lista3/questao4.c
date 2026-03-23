dfbe022589c1c50c6a1e0292528341abba874a86
joaoazevedocavalcanti <joaoazevedocavalcanti@gmail.com>
2026-03-23 16:14:10 -0300

    atualizaçao_questao4

--- lista3/questao4.c
+++ lista3/questao4.c
@@ -8,7 +8,7 @@ int main() {
     scanf("%d", &numero_casos);
 
     int tamanhos[100]; //variavel para armazenar o tamanho dos leds menores
-    char led_grande[10000]; //variavel para ir adicionando o led grande
+    char led_grande[100000]; //variavel para ir adicionando o led grande
 
     //laço geral
     for (int i = 0; i < numero_casos; i++){
@@ -29,11 +29,12 @@ int main() {
             else if (led[0] == 'O'){
                 led[0] = 'X';
                 //laço para realizar a mudança
-                for (int m = 1; m < tamanhos[i]; m++){
-                    if (led[m] == 'X'){
-                        led[m] = 'O';
+                for (int m = 0; m < tamanhos[i]; m++){
+                    if (led[m + 1] == 'O'){
+                        led[m + 1] = 'X';
                     } else {
-                        led[m] = 'X';
+                        led[m+1] = 'O';
+                        break;
                     }
                 }
             }
@@ -53,67 +54,36 @@ int main() {
         }
         else { //caso não seja a primeira
             int tamanho_led_grande = strlen(led_grande); //variavel que vai guardar o tamanho para ajudar na adição
+            char estado_ultimo_led = led_grande[tamanho_led_grande - 1];
+            char fio;
+            //verificação do fio
+            if (tamanho_led_grande % 2 == 0){
+                //condição @
+                if (estado_ultimo_led == led[0]){
+                    fio = '@';
+                }
+                //condição $
+                else {
+                    fio = '$';
+                }
+            } else {
+                if (estado_ultimo_led == led[0]){
+                    fio = '#';
+                }
+                else {
+                    fio = '%';
+                }
+            }
+            led_grande[tamanho_led_grande] = fio;
+            tamanho_led_grande++;
+
             for (int lg = 0; led[lg] != '\0'; lg++){
                 led_grande[tamanho_led_grande + lg] = led[lg];
             } 
-        }
+        }   
     }
-    //verificação dos fios
-    int tamanho_led_grande = strlen(led_grande);
-    char estado_ultimo_led = led_grande[tamanho_led_grande - 1]; //estado do ultimo led
-    char led_grande_finalizado[tamanho_led_grande + numero_casos]; //array final com os leds e os fios
-
-    int qt_fios = 0; //variavel que vai me ajudar a adcionar os fios
-    //laço geral da adicão dos fios, esse verifica o local onde os fios vao ser adicionados
-    for (int f = 0; f < numero_casos; f++){
-        //definindo onde cada laço vai começar e acabar
-        int comeco, final;
-        if (f == 0) {
-            comeco = 0;
-            final = tamanhos[f + 1];
-        } else {
-            comeco = tamanhos[f] + qt_fios;
-            final = tamanhos[f + 1] + qt_fios;
-        }
-
-        //laço para adicionar os fios 
-        for (int add = comeco; add <= final; add++){
-            //caso seja o momento onde eu devo adicionar            
-            if (add == final){
-                //caso seja par 
-                if (tamanho_led_grande % 2 == 0){
-                    //condição @
-                    if(estado_ultimo_led == led_grande[final]){
-                        led_grande_finalizado[add] = '@';
-                        qt_fios++;
-                    }
-                    //condição $
-                    else if (estado_ultimo_led != led_grande[final]){
-                        led_grande_finalizado[add] = '$';
-                        qt_fios++;
-                    }
-                }
-                //caso seja impar 
-                else {
-                    //condição # 
-                    if (estado_ultimo_led == led_grande[final]){
-                        led_grande_finalizado[add] = '#';
-                        qt_fios++;
-                    }
-                    //condição %
-                    else if (estado_ultimo_led != led_grande[final]){
-                        led_grande_finalizado[add] = '%';
-                        qt_fios++;
-                    }
-                }
-            }else {
-                led_grande_finalizado[add] = led_grande[add - qt_fios];
-            }
-        }
-    }   
-    //print final 
-    for (int pf = 0; led_grande_finalizado[pf] != '\0'; pf++){
-        printf("%c", led_grande_finalizado[pf]);
+    for (int plg = 0; led_grande[plg] != '\0'; plg++){
+        printf("%c", led_grande[plg]);
     }
 }
 
